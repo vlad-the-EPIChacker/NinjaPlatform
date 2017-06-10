c4fafb7eef5e5798df5a83b7533d8e41e9a1ecd9
Vlad <vladov3000@gmail.com>
2017-06-10 11:06:37 -0700

    unity

---
+++ Ninja Platform/Assets/Scripts/Camera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+	[SerializeField] private GameObject player;
+	public float smoothTime = -1f;
+	private Vector2 velocity = Vector2.zero;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		Vector2 targetPosition = player.transform.position;
+		transform.position = Vector2.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+		transform.position = new Vector3 (transform.position.x, transform.position.y, -10f); 	
+	
+	}
+		
+}
---
+++ Ninja Platform/Assets/Scripts/Player.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private Animator anim;
+	[SerializeField] private float speed = 1f;
+	[SerializeField] private float jumpForce = 1f;
+	[SerializeField] private float slideSpeed = 1f;
+	private bool touchingFloor = true;
+	private Collider2D playerCollider;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		playerCollider = GetComponent<Collider2D> ();
+		float originalSlideSpeed = slideSpeed;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.localRotation = new Quaternion();
+		if (Input.GetKey (KeyCode.W) && Input.GetKey(KeyCode.D) && touchingFloor==true) {
+			if (transform.localScale.x < 0) {
+				transform.localScale = new Vector3 (transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+			}
+			anim.Play ("Jump");
+			transform.Translate (Vector2.up*Time.deltaTime*jumpForce);
+			transform.Translate (Vector2.right*Time.deltaTime*speed);
+		}
+
+		else if (Input.GetKey (KeyCode.W) && Input.GetKey(KeyCode.A) && touchingFloor==true) {
+			if (transform.localScale.x > 0) {
+				transform.localScale = new Vector3 (transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+			}
+			anim.Play ("Jump");	
+			transform.Translate (Vector2.up*Time.deltaTime*jumpForce);
+			transform.Translate (Vector2.right*Time.deltaTime*speed);
+		}
+
+		else if (Input.GetKey (KeyCode.W) && touchingFloor==true) {
+			anim.Play ("Jump");
+			transform.Translate (Vector2.up*Time.deltaTime*jumpForce);
+		}
+
+		else if (Input.GetKey (KeyCode.S) && Input.GetKey(KeyCode.A)) {
+			if (transform.localScale.x > 0) {
+				transform.localScale = new Vector3 (transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+			}
+			anim.Play ("Slide");	
+			transform.Translate (Vector2.down*Time.deltaTime*speed);
+			transform.Translate (Vector2.right*Time.deltaTime*slideSpeed);
+		}
+
+		else if (Input.GetKey (KeyCode.S) && Input.GetKey(KeyCode.D)) {
+			if (transform.localScale.x < 0) {
+				transform.localScale = new Vector3 (transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+			}
+			anim.Play ("Slide");	
+			transform.Translate (Vector2.down*Time.deltaTime*speed);
+			transform.Translate (Vector2.right*Time.deltaTime*slideSpeed);
+		}
+
+
+		else if (Input.GetKey (KeyCode.D)) {
+			if (transform.localScale.x < 0) {
+				transform.localScale = new Vector3 (transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+			}
+			anim.Play ("Run");
+			transform.Translate (Vector2.right*Time.deltaTime*speed);
+		}
+
+		else if (Input.GetKey (KeyCode.A)) {
+			
+			if (transform.localScale.x > 0) {
+				transform.localScale = new Vector3 (transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+
+			}
+			anim.Play ("Run");
+			transform.Translate (Vector2.right * Time.deltaTime * speed);
+		}
+	}
+		
+
+}
